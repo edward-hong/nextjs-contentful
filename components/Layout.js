8cc090ed5f325e5082c51154cdd4b8ad2062d2e0
Edward Hong <edward.hong527@gmail.com>
2022-05-22 03:10:20 +1200

    Next.js & Contentful Site Build Tutorial #1 - Intro & Setup

--- components/Layout.js
+++ components/Layout.js
@@ -15,9 +15,7 @@ export default function Layout({ children }) {
         </Link>
       </header>
 
-      <div className="page-content">
-        { children }
-      </div>
+      <div className="page-content">{children}</div>
 
       <footer>
         <p>Copyright 2021 Just Add Marmite :)</p>
